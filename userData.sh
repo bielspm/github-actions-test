ab66f807198f6511a1ac3fc9dd88e06e763f6264
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-01-17 12:45:01 -0300

    added appspec.yml

---
+++ userData.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get install python3.10
+python -m pip install --upgrade pip
+pip -m virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+flask --app app-flask.py run --port 80
