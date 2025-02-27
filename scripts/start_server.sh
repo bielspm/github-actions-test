658e0af7a196c105815dd31426c68aadbb43fb5b
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-02-26 23:05:36 -0300

    teste

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 sudo su
 cd /home/github-actions-test
 source venv/bin/activate
-python3.10 -m gunicorn --workers=2 app-flask:app -b 0.0.0.0:80
+sudo gunicorn --workers=2 app-flask:app -b 0.0.0.0:80
