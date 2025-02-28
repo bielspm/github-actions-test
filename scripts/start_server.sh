33283d2858dab633eda34e3396c02cfb2a7ce756
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-02-28 09:35:00 -0300

    iniciando gunicorn com nome qualificado

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 sudo su
 cd /home/github-actions-test
 source venv/bin/activate
-sudo gunicorn --workers=2 app-flask:app -b 0.0.0.0:80
+/home/github-actions-test/venv/bin/gunicorn --workers=2 app-flask:app -b 0.0.0.0:80
