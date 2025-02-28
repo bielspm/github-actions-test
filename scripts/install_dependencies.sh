f57e2bd0fa3d7791555fa00ff760fc4a2f984847
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-02-28 09:46:18 -0300

    stop error file busy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,9 @@
 sudo su
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
-sudo apt-get install python3.10 python3.10-venv git-all -y
+sudo apt-get install python3.10 -y
+sudo apt-get install python3.10-venv -y
+sudo apt-get install git-all -y
 cd /home
 cd github-actions-test
 #python3.10 -m venv venv
