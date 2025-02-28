ed0edfaecf7b46ed671cb82548c8baa1c361aa38
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-02-28 15:08:52 -0300

    iniciando gunicorn com nome qualificado

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,12 +2,13 @@
 sudo su
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
-sudo apt-get install python3.10 -y
-sudo apt-get install python3.10-venv -y
-sudo apt-get install git-all -y
+#sudo apt-get install python3.10 -y
+#sudo apt-get install python3.10-venv -y
+#sudo apt-get install git-all -y
 cd /home
 cd github-actions-test
 #python3.10 -m venv venv
+export PATH=$PATH:/home/github-actions-test/venv/bin/
 source venv/bin/activate
 pip install -r requirements.txt
 apt remove apache2 -y
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -ex
-#sudo su
-cd /home/github-actions-test
-source venv/bin/activate
+sudo su
+cd /home/github-actions-test && source venv/bin/activate
 /home/github-actions-test/venv/bin/gunicorn --workers=2 app-flask:app -b 0.0.0.0:80
