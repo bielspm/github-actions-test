e21d1c99e8970f4d0386b39cc1cb63ad8dd0ba2f
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-02-28 17:29:40 -0300

    iniciando gunicorn com nome qualificado

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,7 +7,8 @@ sudo apt-get update -y
 #sudo apt-get install git-all -y
 #cd /home
 #cd github-actions-test
-cd /home/github-actions-test && source venv/bin/activate
+cd /home/github-actions-test
+source venv/bin/activate
 #python3.10 -m venv venv
 echo "venv criado"
 pip install -r requirements.txt
