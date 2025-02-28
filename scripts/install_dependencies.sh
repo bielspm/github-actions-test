d0f815a531718f077a45627850d8462a93c1ce9d
gabriel barbosa <gabrielgbsspm@hotmail.com>
2025-02-28 18:22:56 -0300

    testando sem sudo su

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -ex
 #sudo su
-#sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
-#sudo apt-get install python3.10 -y
-#sudo apt-get install python3.10-venv -y
-#sudo apt-get install git-all -y
+sudo apt-get install python3.10 -y
+sudo apt-get install python3.10-venv -y
+sudo apt-get install git-all -y
 #cd /home
 #cd github-actions-test
 cd /home/github-actions-test
+echo $(ls)
+python3.10 -m venv venv
 . venv/bin/activate
-#python3.10 -m venv venv
 echo "venv ativado"
 pip install -r requirements.txt
 echo "instalando requirements.txt"
