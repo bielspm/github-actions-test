d7528d49b4c6c702a401c33cb2825ee6777ecf4c
gabriel barbosa <gabrielgbsspm@hotmail.com>
2026-03-11 16:22:13 -0300

    rota removida

--- app-flask.py
+++ app-flask.py
@@ -14,12 +14,6 @@ def home():
 def about():
     return jsonify({"version": "1.0", "author": "Yagmur Ozden"}), 200
 
-@app.route('/shutdown', methods=['POST'])
-def shutdown():
-   print("Shutting down gracefully...")
-   os.kill(os.getpid(), signal.SIGINT)
-   return 'Server shutting down...'
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
